0803d458b801f37d5f395ea25f628cf57a291a0d
Mateusz <226069@student.pwr.edu.pl>
2019-12-04 18:30:20 +0100

    Initial commit

---
+++ app/src/main/java/com/mattech/task_list/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mattech.task_list;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
