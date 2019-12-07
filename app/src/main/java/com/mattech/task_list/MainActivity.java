573d207f06499b447c424e1c43174651e8e7219d
Mateusz <226069@student.pwr.edu.pl>
2019-12-07 22:58:16 +0100

    ViewModel used to provide data for recycler view adapter. Activity changed to AppCompatActivity that implements LifecycleOwner which is necessary to get a viewmodel

--- app/src/main/java/com/mattech/task_list/MainActivity.java
+++ app/src/main/java/com/mattech/task_list/MainActivity.java
@@ -1,13 +1,38 @@
 package com.mattech.task_list;
 
-import android.app.Activity;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends Activity {
+import com.mattech.task_list.adapters.TaskAdapter;
+import com.mattech.task_list.models.Task;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements TaskAdapter.TaskActionListener {
+    private TaskViewModel viewModel;
+
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        viewModel = ViewModelProviders.of(this).get(TaskViewModel.class);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        TaskAdapter taskAdapter = new TaskAdapter(this);
+        recyclerView.setAdapter(taskAdapter);
+        viewModel.getTasks().observe(this, taskAdapter::setTasks);
+    }
+
+    @Override
+    public void taskStatusChanged(Task task) {
+        viewModel.update(task);
     }
 }
