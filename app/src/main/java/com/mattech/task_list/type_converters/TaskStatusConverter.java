21f93d937f0db90dada05b634174a8f9185bbc1f
Mateusz <226069@student.pwr.edu.pl>
2020-09-09 23:08:35 +0200

    Missing @NonNull annotation added

--- app/src/main/java/com/mattech/task_list/type_converters/TaskStatusConverter.java
+++ app/src/main/java/com/mattech/task_list/type_converters/TaskStatusConverter.java
@@ -1,13 +1,14 @@
 package com.mattech.task_list.type_converters;
 
 import android.arch.persistence.room.TypeConverter;
+import android.support.annotation.NonNull;
 
 import com.mattech.task_list.models.Task;
 
 public class TaskStatusConverter {
 
     @TypeConverter
-    public static String statusToString(Task.TaskStatus taskStatus) {
+    public static String statusToString(@NonNull Task.TaskStatus taskStatus) {
         return taskStatus.name();
     }
 
